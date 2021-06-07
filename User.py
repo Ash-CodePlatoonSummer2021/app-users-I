3ee150d28d682d8658fae01db2362e190b81f703
asriniv0 <ashsrinivasan1211@gmail.com>
2021-06-06 23:09:52 -0500

    User

--- User.py
+++ User.py
@@ -1,1 +1,7 @@
 # your User class goes here
+
+class User:
+    def __init__(self, name, emailAddress, licenseNmbr):
+        self.name=name
+        self.emailAddress=emailAddress
+        self.licenseNmbr= licenseNmbr
